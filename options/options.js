cd47da454ee4da713e19afd0bdea4a97647d3eb2
Crowdbotics <team@crowdbotics.com>
2021-09-24 06:09:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "yellow_surf_30814",
+  url: "https://yellow_surf_30814.botics.co",
+  api: "https://yellow_surf_30814.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
